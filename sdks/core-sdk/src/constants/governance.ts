3316bef0c3cde3660d6753b0bb6cefe9d4c73f33
0xLightt <0xlightt@protonmail.com>
2024-07-18 18:07:37 +0100

    fix(maia-core-sdk): Add governance contracts

---
+++ sdks/core-sdk/src/constants/governance.ts
@@ -0,0 +1,96 @@
+import { ZERO_ADDRESS } from './addresses'
+import { SupportedChainId } from './chainIds'
+import { AddressMap } from './types'
+
+/**
+ * The latest Maia governor bravo
+ */
+export const GOVERNANCE_BRAVO_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0xEa45E41163D5eAC67dF40D88db4C8985D22Fc84f',
+}
+
+/**
+ * The latest hermes governor bravo for severity 1 governance proposals
+ */
+export const GOVERNANCE_BRAVO_SEVERITY_1_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x415F25DbeeC0f1A29D69cCDCF9E475Bb60592E9d',
+}
+
+/**
+ * The latest hermes governor bravo for severity 2 governance proposals
+ */
+export const GOVERNANCE_BRAVO_SEVERITY_2_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x32F8Fb0723F20346DbA485ccdCa6444b59239a7F',
+}
+
+/**
+ * The latest hermes governor bravo for severity 3 governance proposals
+ */
+export const GOVERNANCE_BRAVO_SEVERITY_3_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x4aA5b20A25EEFbcd6a78E9b04de0838E2F1c978E',
+}
+
+/**
+ * The latest hermes governor bravo for severity 4 governance proposals
+ */
+export const GOVERNANCE_BRAVO_SEVERITY_4_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x983319a18E4d5602714232EFfa73C53440aA416B',
+}
+
+/**
+ * The latest hermes governor bravo for severity 5 governance proposals
+ */
+export const GOVERNANCE_BRAVO_SEVERITY_5_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x73a423DF7A9e118Af53E9E1Cf820B11ea4617494',
+}
+
+export const TIMELOCK_ADDRESS: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x865dBDbDa91F736B40347fcC7B8e0c5616C21b48',
+}
+
+/**
+ * The latest hermes governor bravo for severity 1 governance proposals
+ */
+export const TIMELOCK_SEVERITY_1_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0xC037Baeaf3e7fc2A5C7Ab804502B69E03Dd91a1f',
+}
+
+/**
+ * The latest hermes governor bravo for severity 2 governance proposals
+ */
+export const TIMELOCK_SEVERITY_2_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x4906bb908d2a1F424bd3D9457582C77bdb679b76',
+}
+
+/**
+ * The latest hermes governor bravo for severity 3 governance proposals
+ */
+export const TIMELOCK_SEVERITY_3_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0xF80273b0BFde92398BE94656f16A3F1910D84645',
+}
+
+/**
+ * The latest hermes governor bravo for severity 4 governance proposals
+ */
+export const TIMELOCK_SEVERITY_4_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x49414F35D860ee777EbE39Ee57f9D76aD2c1Dc1F',
+}
+
+/**
+ * The latest hermes governor bravo for severity 5 governance proposals
+ */
+export const TIMELOCK_SEVERITY_5_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.SEPOLIA]: '0x062E5D0E5180D4313B590d70157707c58b66018B',
+}
