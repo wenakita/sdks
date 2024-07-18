abe885f0aa1618a607cc558ee062924a59da7e35
0xLightt <0xlightt@protonmail.com>
2024-07-18 19:48:19 +0100

    fix(hermes-v2-sdk): Export FeeAmount

--- sdks/hermes-v2-sdk/src/index.ts
+++ sdks/hermes-v2-sdk/src/index.ts
@@ -2,13 +2,14 @@ export {
   ADDRESS_ZERO,
   BALANCER_VAULT_ADDRESS,
   FACTORY_ADDRESS,
+  FeeAmount,
   POOL_INIT_CODE_HASH,
   Rounding,
   TICK_SPACINGS,
   TradeType,
   VAULT_FACTORY_ADDRESS,
 } from './constants/constants'
-export { MINIMUM_LIQUIDITY, Q96, Q192 } from './constants/internalConstants'
+export { MINIMUM_LIQUIDITY, Q192, Q96 } from './constants/internalConstants'
 export * from './entities'
 export * from './types'
 export * from './utils'
