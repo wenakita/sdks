22ec68367ecdaa2b44b8dfe61841692414a7b962
0xLightt <0xlightt@protonmail.com>
2024-07-23 19:52:56 +0100

    fix compute virtual account test

--- sdks/ulysses-sdk/src/utils/computeVirtualAccount.test.ts
+++ sdks/ulysses-sdk/src/utils/computeVirtualAccount.test.ts
@@ -3,13 +3,13 @@ import { SupportedChainId } from 'maia-core-sdk'
 import { computeVirtualAccount } from './computeVirtualAccount'
 
 describe('computeVirtualAccount', () => {
-  const ADDRESS_TEST_ONE = '0x03f27017e164f9bf4c9fc0a52a26f1b65cbe970e'
+  const ADDRESS_TEST_ONE = '0x88E07a0457aA113AB910103d9a01217315DA1C98'
   const ADDRESS_TEST_TWO = '0xC466af7ff16ef0f1A7fa4E23E095E47a4058D791'
 
   describe('#compute', () => {
     it('compute works fine testnet', () => {
-      const VIRTUAL_ACCOUNT_TESTNET_ONE = '0x17818160B6eE49656Adc9528131b6D33fd95642F'
-      const VIRTUAL_ACCOUNT_TESTNET_TWO = '0x066DFbFcFBb73912F7eA48675584FaDdB215983d'
+      const VIRTUAL_ACCOUNT_TESTNET_ONE = '0x6639b02F5caf6cF25a8FF54246Fa988768126112'
+      const VIRTUAL_ACCOUNT_TESTNET_TWO = '0x286aC68E8c5DA673D222807da61e671C621f2a99'
 
       const testRootChainId = SupportedChainId.SEPOLIA
 
