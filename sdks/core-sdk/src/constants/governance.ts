7e8c20d95085a3c9c95893259d01a4ec2f38a2f6
0xLightt <0xlightt@protonmail.com>
2024-08-13 02:09:39 +0100

    fix(core-sdk): add last arb addresses - gov contracts

--- sdks/core-sdk/src/constants/governance.ts
+++ sdks/core-sdk/src/constants/governance.ts
@@ -1,4 +1,3 @@
-import { ZERO_ADDRESS } from './addresses'
 import { SupportedChainId } from './chainIds'
 import { AddressMap } from './types'
 
@@ -6,7 +5,7 @@ import { AddressMap } from './types'
  * The latest Maia governor bravo
  */
 export const GOVERNANCE_BRAVO_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0x2d5A3876468d476626862B0dea1513fC051972b8',
   [SupportedChainId.SEPOLIA]: '0x61B046e0812CF9c170fa09b9834708F73421B59b',
 }
 
@@ -14,7 +13,7 @@ export const GOVERNANCE_BRAVO_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 1 governance proposals
  */
 export const GOVERNANCE_BRAVO_SEVERITY_1_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0x9572c2695D6d77e4a2Ee0def06C01D65dBC0dD9b',
   [SupportedChainId.SEPOLIA]: '0x28a9ffd6Fe4497f0788606F64Ab7095fDeCe4Ebb',
 }
 
@@ -22,7 +21,7 @@ export const GOVERNANCE_BRAVO_SEVERITY_1_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 2 governance proposals
  */
 export const GOVERNANCE_BRAVO_SEVERITY_2_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0x2030fBE61A54862E019baa75aAd40bfA2E55Cff4',
   [SupportedChainId.SEPOLIA]: '0x38bAd91B4c7016f8a9C71aab1071769Ec0FBe241',
 }
 
@@ -30,7 +29,7 @@ export const GOVERNANCE_BRAVO_SEVERITY_2_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 3 governance proposals
  */
 export const GOVERNANCE_BRAVO_SEVERITY_3_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0xd9c8f1cA64952dF35A0A46366BA5F2e3Fb47c694',
   [SupportedChainId.SEPOLIA]: '0x8d2c9aA2e899B18e03931ccFcf088be0b3a23BdE',
 }
 
@@ -38,7 +37,7 @@ export const GOVERNANCE_BRAVO_SEVERITY_3_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 4 governance proposals
  */
 export const GOVERNANCE_BRAVO_SEVERITY_4_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0xa363FA2b0e4F2E831DCfE259eF253cFe3967578F',
   [SupportedChainId.SEPOLIA]: '0xB9618958c953D67E15Bf7eFe78478Dd1e8b23CeC',
 }
 
@@ -46,12 +45,15 @@ export const GOVERNANCE_BRAVO_SEVERITY_4_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 5 governance proposals
  */
 export const GOVERNANCE_BRAVO_SEVERITY_5_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0x2d4A2817D4bD8efFD21DBd1679664E7C0F288f38',
   [SupportedChainId.SEPOLIA]: '0xBFFc42560a51832daa466955f39D81915402Fd50',
 }
 
+/**
+ * The latest maia governor bravo for governance proposals
+ */
 export const TIMELOCK_ADDRESS: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0xaC8B685A187531D56af0aE4f0EC4A8a4743B4b27',
   [SupportedChainId.SEPOLIA]: '0x86712f780F8883482e7EBf6f395CEe374CC59787',
 }
 
@@ -59,7 +61,7 @@ export const TIMELOCK_ADDRESS: AddressMap = {
  * The latest hermes governor bravo for severity 1 governance proposals
  */
 export const TIMELOCK_SEVERITY_1_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0xcb73DCA22eD2c4059902d8dd2C9E61c30fB4E48B',
   [SupportedChainId.SEPOLIA]: '0x16DfceFF2e21c54Cd4B262edDCc1baEB734a112A',
 }
 
@@ -67,7 +69,7 @@ export const TIMELOCK_SEVERITY_1_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 2 governance proposals
  */
 export const TIMELOCK_SEVERITY_2_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0x25b329A0205EE82fDd053ca69818Ff9894beB229',
   [SupportedChainId.SEPOLIA]: '0x0d5783E0e19bf09A923A742E5feB0653c2Eed638',
 }
 
@@ -75,7 +77,7 @@ export const TIMELOCK_SEVERITY_2_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 3 governance proposals
  */
 export const TIMELOCK_SEVERITY_3_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0x13E8e8531AD6AF45950aa73B6d718D8560613B9d',
   [SupportedChainId.SEPOLIA]: '0x69ff16440052DE228a158517d91E92D803e072D7',
 }
 
@@ -83,7 +85,7 @@ export const TIMELOCK_SEVERITY_3_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 4 governance proposals
  */
 export const TIMELOCK_SEVERITY_4_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0x272b1083cad5F3C85d3687ABEA8fAB9A709116c1',
   [SupportedChainId.SEPOLIA]: '0x8e0881066dA3B81041105B58303a975bD507091C',
 }
 
@@ -91,6 +93,6 @@ export const TIMELOCK_SEVERITY_4_ADDRESSES: AddressMap = {
  * The latest hermes governor bravo for severity 5 governance proposals
  */
 export const TIMELOCK_SEVERITY_5_ADDRESSES: AddressMap = {
-  [SupportedChainId.ARBITRUM_ONE]: ZERO_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: '0xE136289bBe077cD714F600B539286762319A3Ec4',
   [SupportedChainId.SEPOLIA]: '0x9de0F9B4a94a2119789F03A21Da42576114A0554',
 }
