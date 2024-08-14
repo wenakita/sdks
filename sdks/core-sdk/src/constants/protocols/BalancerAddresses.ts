292d1c7c3d86474bb7f23ae83ebe6e6cab0eb7ef
0xLightt <0xlightt@protonmail.com>
2024-08-14 01:50:46 +0100

    fix(core-sdk): fix balancer addresses structure

--- sdks/core-sdk/src/constants/protocols/BalancerAddresses.ts
+++ sdks/core-sdk/src/constants/protocols/BalancerAddresses.ts
@@ -6,6 +6,7 @@ import { SupportedChainId } from '../chainIds'
  */
 export const ArbitrumBalancerAddresses: IBalancer = {
   ComposableStablePoolWrapperFactory: '0x00Dccc0053C2Fa9b0000085a00bD15F200961af0',
+  ComposableStablePoolWrapperInitCodeHash: '0x2f29c1929bc70aae113b1671d9250cc3e2ab673d22188542ebf28ded1ac9e1a4', // TODO Change to real init code hash
 }
 
 /**
@@ -13,6 +14,7 @@ export const ArbitrumBalancerAddresses: IBalancer = {
  */
 export const SepoliaBalancerAddresses: IBalancer = {
   ComposableStablePoolWrapperFactory: '0x996AAA029f3A8826C22CcCf6127A16A0e52FC3Da',
+  ComposableStablePoolWrapperInitCodeHash: '0x2f29c1929bc70aae113b1671d9250cc3e2ab673d22188542ebf28ded1ac9e1a4',
 }
 
 export const BalancerAddresses: BalancerAddressesType = {
--- sdks/core-sdk/src/types/protocols/balancer.ts
+++ sdks/core-sdk/src/types/protocols/balancer.ts
@@ -2,6 +2,7 @@ import { IProtocolAddresses, ProtocolAddressesType } from '../basic'
 
 export interface IBalancer extends IProtocolAddresses {
   ComposableStablePoolWrapperFactory: string
+  ComposableStablePoolWrapperInitCodeHash: string
 }
 
 export type BalancerAddressesType = ProtocolAddressesType<IBalancer>
