cc07ba72abb5316ce9b497a56dc0574ed517b835
0xLightt <0xlightt@protonmail.com>
2024-08-22 21:37:00 +0100

    fix(router-sdk): update universal router sdk

--- sdks/router-sdk/src/utils/constants.ts
+++ sdks/router-sdk/src/utils/constants.ts
@@ -3,7 +3,7 @@ import { ZERO_ADDRESS } from 'maia-core-sdk'
 export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
   switch (chainId) {
     case 42161: // arbitrum
-      return '0x0000000003Af65ffb293E722e9Fe41e9C15ABB3D'
+      return '0x00000006f4dd9687c729a608a0ddb70400001095'
     case 11155111: // sepolia
       return '0x5181bd48C2070E1f850C4a1F59665f02E4617Cb0'
     case 1: // mainnet
